6b00b30dc0f7881dded6bcc8cfd939837e53b359
Aluno <anagimenes1411@gmail.com>
2018-10-15 09:40:51 -0300

    Cadastro Fornecedor

--- fornecedor.php
+++ fornecedor.php
@@ -4,120 +4,111 @@
     <meta charset="utf-8">
     <title>Cadastro Fornecedor</title>
   </head>
-  <body>
 
+  <style type="text/css">
 
-<style type="text/css">
-<!--
-.style1 {
-color: #FF0000;
-font-size: x-small;
-}
-.style3 {color: #0000FF; font-size: x-small; }
-</style>
-<script type="text/javascript">
-function validaCampo()
-{
-if(document.cadastro.nome.value=="")
-{
-alert("O Campo nome é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.email.value=="")
-{
-alert("O Campo email é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.endereco.value=="")
-{
-alert("O Campo endereço é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.cidade.value=="")
-{
-alert("O Campo Cidade é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.estado.value=="")
-{
-alert("O Campo Estado é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.bairro.value=="")
-{
-alert("O Campo Bairro é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.pais.value=="")
-{
-alert("O Campo país é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.login.value=="")
-{
-alert("O Campo Login é obrigatório!");
-return false;
-}
-else
-if(document.cadastro.senha.value=="")
-{
-alert("Digite uma senha!");
-return false;
-}
-else
-return true;
-}
-<!-- Fim do JavaScript que validará os campos obrigatórios! -->
-</script>
+    .style1 {
+    color: #FF0000;
+    font-size: x-small;
+    }
+
+    .style3 {color: #0000FF; font-size: x-small;
+    }
+
+  </style>
+
+<!-- Validação Campos -->
+  <script type="text/javascript">
+    function validaCampo()
+    {
+    if(document.cadastro.nome.value==""){
+    alert("O Campo Nome é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.documento.value==""){
+    alert("O Campo Documento é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.contato.value==""){
+    alert("O Campo Contato é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.estado.value==""){
+    alert("O Campo Estado é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.cidade.value==""){
+    alert("O Campo Cidade é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.cep.value==""){
+    alert("O Campo CEP é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.rua.value==""){
+    alert("O Campo Rua/Avenida é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.bairro.value==""){
+    alert("O Campo Bairro é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.numero.value==""){
+    alert("O Campo Número é obrigatório!");
+    return false;
+    }
+    else
+    if(document.cadastro.email.value=="")
+    {
+    alert("O Campo email é obrigatório!");
+    return false;
+    }
+    else
+    return true;
+    }
+  </script>
 
 <body>
 <form id="cadastro" name="cadastro" method="post" action="cadastro.php" onsubmit="return validaCampo(); return false;">
+
   <table width="625" border="0">
     <tr>
+
       <td width="69">Nome:</td>
-      <td width="546"><input name="nome" type="text" id="nome" size="70" maxlength="60" />
-        <span class="style1">*</span></td>
-    </tr>
-    <tr>
-      <td>Email:</td>
-      <td><input name="email" type="text" id="email" size="70" maxlength="60" />
+      <td width="546"><input name="nome" type="text" id="nome" size="70" maxlength="255" />
       <span class="style1">*</span></td>
+
     </tr>
+
     <tr>
-      <td>Sexo:</td>
-      <td><input name="sexo" type="radio" value="Masculino" checked="checked" />
-        Masculino
-        <input name="sexo" type="radio" value="Feminino" />
-        Feminino <span class="style1">*</span> </td>
+
+      <td width="69">Documento:</td>
+      <td width="546"><input name="documento" type="text" id="documento" size="70" maxlength="255" />
+      <span class="style1">*</span></td>
+
     </tr>
+
     <tr>
-      <td>DDD:</td>
-      <td><input name="ddd" type="text" id="ddd" size="4" maxlength="2" />
-      Telefone:
-        <input name="telefone" type="text" id="telefone" />
-        <span class="style3">Apenas números</span> </td>
+
+      <td width="69">Contato:</td>
+      <td width="546"><input name="contato" type="text" id="contato" size="55" maxlength="255" />
+      <span class="style1">*</span> <span class="style3">Apenas números</span> </td>
+
     </tr>
+
     <tr>
-      <td>Endereço:</td>
-      <td><input name="endereco" type="text" id="endereco" size="70" maxlength="70" />
-        <span class="style1">*</span></td>
-    </tr>
-    <tr>
-      <td>Cidade:</td>
-      <td><input name="cidade" type="text" id="cidade" maxlength="20" />
-        <span class="style1">*</span></td>
-    </tr>
-    <tr>
-      <td>Estado:</td>
-      <td><select name="estado" id="estado">
-        <option>Selecione...</option>
+
+      <td width="69">Estado:</td>
+      <td><select name="estado" id="estado" >
+        <option >Selecione...</option>
         <option value="AC">AC</option>
         <option value="AL">AL</option>
         <option value="AP">AP</option>
@@ -144,36 +135,66 @@ return true;
         <option value="SP">SP</option>
         <option value="SE">SE</option>
         <option value="TO">TO</option>
-          </select>
-        <span class="style1">*      </span></td>
+        </select>
+        <span class="style1">*</span></td>
+
     </tr>
+
     <tr>
-      <td>Bairro:</td>
-      <td><input name="bairro" type="text" id="bairro" maxlength="20" />
+
+      <td width="69">Cidade:</td>
+      <td><input name="cidade" type="text" id="cidade" size="70" maxlength="255" />
+      <span class="style1">*</span></td>
+
+    </tr>
+
+    <tr>
+
+      <td width="69">CEP:</td>
+      <td><input name="cep" type="text" id="cep" maxlength="50" />
+      <span class="style1">*</span></td>
+
+    </tr>
+
+    <tr>
+
+      <td width="69">Rua/Avenida:</td>
+      <td width="546"><input name="rua" type="text" id="rua" size="70" maxlength="255" />
+      <span class="style1">*</span></td>
+
+    </tr>
+
+    <tr>
+
+      <td width="69">Bairro:</td>
+      <td><input name="bairro" type="text" id="bairro" size="42" maxlength="255"/>
+      <span class="style1">*</span>
+      Número:
+        <input name="numero" type="text" id="numero" size="10" maxlength="255"/>
+        <span class="style1">*</span>
+       </td>
+
+    </tr>
+
+    <tr>
+      <td width="69">Email:</td>
+      <td><input name="email" type="text" id="email" size="70" maxlength="255" />
         <span class="style1">*</span></td>
     </tr>
-    <tr>
-      <td>País:</td>
-      <td><input name="pais" type="text" id="pais" maxlength="20" />
-        <span class="style1">*</span></td>
-    </tr>
 
     <tr>
-      <td colspan="2"><input name="news" type="checkbox" id="news" value="ATIVO" checked="checked" />
-Desejo receber novidades e informações sobre o conteúdo deste site. </td>
+
+    <td colspan="2"><p>
+      <input name="cadastrar" type="submit" id="cadastrar" value="Concluir"/>
+
+      <input name="limpar" type="reset" id="limpar" value="Limpar Campos preenchidos!" />
+
+      <span class="style1">* Campos com * são obrigatórios!</span></p></td>
+
     </tr>
-    <tr>
-      <td colspan="2"><p>
-        <input name="cadastrar" type="submit" id="cadastrar" value="Concluir meu Cadastro!" />
 
-
-          <input name="limpar" type="reset" id="limpar" value="Limpar Campos preenchidos!" />
-
-
-          <span class="style1">* Campos com * são obrigatórios!          </span></p>
-      <p>  </p></td>
-    </tr>
   </table>
+
 </form>
   </body>
 </html>
